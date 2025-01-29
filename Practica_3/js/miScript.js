102eeb3d3b420529fe34d0d806dd734e2601b45d
Frangnzlz <151971568+Frangnzlz@users.noreply.github.com>
2025-01-29 12:09:03 +0100

    filtro y codigo

--- Practica_3/js/miScript.js
+++ Practica_3/js/miScript.js
@@ -17,18 +17,39 @@ $(document).ready(function () {
     
     $("section > button").click(function(){
         const table = $(this).prev();
-        let registros = table.find("tr");
-        // console.log(registros.remove());
-        for(let i = registros.length - 1; i > 0; i--){
-            $(registros[i]).remove();
+        let registros = table.find("tr").not(":first");
+        registros.remove()        
+    })
+
+    $(".buscador input").on("keyup", function(){
+        let val = $(this).val()
+        //**Por como esta estructuado mi html, necesito buscar el siguiente hermano del padre 
+        // del input y saltarme el primer tr que se encuentre */
+        $(this).parent().next().find("tr").not(":first").filter(function(){
+            // **Comprueba si se encuentra el texto actual del input dentro del listado del campo codigo
+            // El campo codigo se trata del primer hijo del registro*/
+            let existe = $(this).children().first().text().includes(val);  
+            $(this).toggle(existe)
+        })
+
+    })
+        
+    function generarCodigos(){
+        let chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
+        let resultado = "";
+        for(let i = 0; i < 6; i++) {
+            resultado += chars.charAt(Math.floor(Math.random() * chars.length));
         }
+        return resultado
+    }
 
+    let tablas = $("table")
+    for(let i = 0; i < tablas.length; i++){
+        let tr = $(tablas[i]).find("tr").not(":first")
 
-        // celdas.remove();
-        
-    })
-
-    $(".buscador input").on("keypress", function(){
-        
-    })
+        for(let j = 0; j < tr.length; j++){
+            let td = $(tr[j]).children().first();
+            $(td).text(generarCodigos);
+        }
+    }
 });
